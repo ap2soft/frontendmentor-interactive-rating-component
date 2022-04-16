a26bf2417a5adc0c2899dc13fff85dd3e55318fc
Andrew P <andreich1980@gmail.com>
2022-04-16 22:43:07 +0300

    Task

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-import './tailwind.css'
+import "./tailwind.css";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("rating", () => ({
+    currentRate: null,
+    rates: Array.from({ length: 5 }, (v, key) => key + 1),
+    rated: false,
+
+    submit() {
+      if (!this.currentRate) return;
+
+      this.rated = true;
+    },
+  }));
+});
+
+Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
-// const colors = require('tailwindcss/colors')
-
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
+    screens: {
+      desktop: "1440px",
+    },
+    colors: {
+      orange: "hsl(25, 97%, 53%)",
+      white: "hsl(0, 0%, 100%)",
+      "light-gray": "hsl(217, 12%, 63%)",
+      gray: "hsl(216, 12%, 54%)",
+      blue: "hsl(213, 19%, 18%)",
+      "dark-blue": "hsl(216, 12%, 8%)",
+    },
+    fontFamily: {
+      serif: ["Overpass", "sans-serif"],
+    },
+    fontWeight: {
+      normal: 400,
+      bold: 700,
+    },
     extend: {},
   },
   plugins: [],
-}
+};
